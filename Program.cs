c75bc2f91c2cfa470a87f4dfc8e8014db6a07dcc
sebbe <sebbe@DESKTOP-DHCGTGB>
2023-03-13 22:18:15 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace WordFrequency
+{
+    /// <summary>
+    /// Sebastian Arledal
+    /// CGI codetask
+    /// 13/03/2023
+    /// 
+    /// Simple program to receive a textfile and save the 10 most occurred words from that file and print how many times each word appeared
+    /// 
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WordCalculator wc = new WordCalculator();
+            wc.CalculateWords();
+        }
+
+        
+    }
+}
---
+++ WordCalculator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordFrequency
+{
+    /// <summary>
+    /// Simple class to count words in a text and save the number of occurence for each word
+    /// </summary>
+    internal class WordCalculator
+    {
+        /// <summary>
+        /// Calculates the amount of times a word occured in a text
+        /// </summary>
+        public void CalculateWords()
+        {
+            string text = File.ReadAllText("text.txt");
+
+            char[] delimiter = { ' ', '.', ',', ';', ':', '?', '\n', '\r', '\t', '"', '(', ')', '-' };
+            string[] words = text.Split(delimiter);
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+
+            foreach(string word in words)
+            {
+                //Converts all the words to lowercase
+                string lowerCaseWord = word.ToLower();
+
+                if (word.Length >= 2)
+                {
+                    if (wordCounts.ContainsKey(lowerCaseWord))
+                    {
+                        wordCounts[lowerCaseWord]++;
+                    }
+                    else if (!wordCounts.ContainsKey(lowerCaseWord))
+                    {
+                        wordCounts.Add(lowerCaseWord, 1);
+                    }
+                }                
+            }
+            //Sort by word occurence
+            var sortedWordCounts = wordCounts.OrderByDescending(x => x.Value);
+
+            PrintWords(sortedWordCounts);
+        }
+
+        /// <summary>
+        /// Prints out the 10 most common words in the text and the number of occurrences 
+        /// </summary>
+        /// <param name="d"></param>
+        private void PrintWords(IOrderedEnumerable<KeyValuePair<string, int>> d)
+        {
+            int count = 0;
+            foreach (KeyValuePair<string, int> word in d)
+            {
+                if (count == 10)
+                    return;
+                Console.WriteLine("{0}: {1}", word.Key, word.Value);
+                count++;
+            }        
+        }
+    }
+}
